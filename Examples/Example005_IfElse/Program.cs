cb789ea213c12b5e1fe16ba8c8598d2f86d5b9e1
FlameFrog <revec1251@gmail.com>
2022-10-12 20:17:06 +0300

    added 005 HelloAlex

---
+++ Examples/Example005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+    if (username.ToLower() == "alex")
+    {
+        Console.WriteLine("Здоров, Бро!");
+    }
+    else
+    {
+        Console.Write("Привет, ");
+        Console.Write(username);
+    }
