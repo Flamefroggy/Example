4cb301928f4e0f8329b4d733bda680a261a0df80
FlameFrog <revec1251@gmail.com>
2022-10-12 20:08:45 +0300

    Slightly changes 4

--- Examples/Example004_Div/Program.cs
+++ Examples/Example004_Div/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double number1 = 13;
+double number2 = 2;
+Console.WriteLine(number1 / number2);
