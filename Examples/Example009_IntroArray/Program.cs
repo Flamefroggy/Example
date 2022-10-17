075edca73a35dd16d3a7e574f6ba7a5f0c27a08a
FlameFrog <revec1251@gmail.com>
2022-10-17 15:58:54 +0300

    Array

---
+++ Examples/Example009_IntroArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {42,94,12,2,77,5,34,268,34, 92};
+int n = array.Length;
+int find = 34;
+
+int i = 0;
+
+while (i<n)
+{
+    if (array[i] == find)
+    {
+        Console.WriteLine(i);
+        break;
+    }
+    i++;
+}
+
