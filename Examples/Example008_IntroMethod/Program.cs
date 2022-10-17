3ac1affe95b36209698dbc461da84d67a3d303ae
FlameFrog <revec1251@gmail.com>
2022-10-17 15:31:55 +0300

    Function

---
+++ Examples/Example008_IntroMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 42;
+int a2 = 94;
+int a3 = 12;
+int b1 = 2;
+int b2 = 77;
+int b3 = 5;
+int c1 = 96;
+int c2 = 324;
+int c3 = 23;
+
+int max = Max(
+    Max(a1,a2,a3),
+    Max(b1,b2,b3),
+    Max(c1,c2,c3));
+
+
+Console.WriteLine(max);
